37ac414958c12cc96fe124cf526f31271d61d3b1
Teja Bhavani <tejabhavani597@gmail.com>
2025-07-13 10:20:21 +0000

    commit

--- snapshots/stg_customers_snapshot.sql
+++ snapshots/stg_customers_snapshot.sql
@@ -5,10 +5,10 @@
     target_schema='ANALYTICS',
     strategy='check',
     unique_key='customer_id',
-    check_cols=[ 'age_group', 'insured_sex']  -- Updated to include new columns
+    check_cols=['age_group', 'insured_sex']
   ) 
 }}
-
+-- Comment moved here: Monitoring changes in age_group and insured_sex
 SELECT * FROM {{ ref('stg_customers') }}
 
 {% endsnapshot %}
