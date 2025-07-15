79c404e90079a04985e223de2f06bec94c680efa
Teja Bhavani <tejabhavani597@gmail.com>
2025-07-15 15:52:57 +0000

    commit changes

--- models/analytics/dim_customers.sql
+++ models/analytics/dim_customers.sql
@@ -1,5 +1,14 @@
-{{ config(materialized='table', schema='ANALYTICS') }}
+{{ config(
+    materialized='table',
+    schema='ANALYTICS'
+) }}
 
+WITH latest_snapshot AS (
+  SELECT *,
+         ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY dbt_valid_from DESC) AS rn
+  FROM {{ ref('stg_customers_snapshot') }}
+  WHERE dbt_valid_to IS NULL OR dbt_valid_to > CURRENT_DATE
+)
 SELECT
   customer_id,
   age,
@@ -10,5 +19,8 @@ SELECT
   insured_hobbies,
   insured_relationship,
   capital_gains,
-  capital_loss
-FROM {{ ref('stg_customers') }}
+  capital_loss,
+  dbt_valid_from AS effective_date,
+  COALESCE(dbt_valid_to, '9999-12-31') AS end_date
+FROM latest_snapshot
+WHERE rn = 1
